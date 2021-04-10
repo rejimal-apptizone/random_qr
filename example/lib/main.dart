89de387fd56bfe446e2606654360b43e2870bf7b
Antony Rejimal <dev.rejimal@gmail.com>
2021-04-10 15:16:33 +0530

    firebae config added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:randnumber/randnumber.dart';
 import 'package:randnumber_example/ui/screens/dashboard/dashboard_screen.dart';
 
-import 'ui/screens/login/login_screen.dart';
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
