467fc52de5d65b3b8b0466cd2718cded40cff006
Antony Rejimal <dev.rejimal@gmail.com>
2021-04-10 17:43:27 +0530

    code refactored

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,10 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:randnumber/randnumber.dart';
-import 'package:randnumber_example/ui/screens/dashboard/dashboard_screen.dart';
 import 'package:randnumber_example/ui/screens/login/login_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
